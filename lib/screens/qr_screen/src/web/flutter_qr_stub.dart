93feefdee9020c2135823ccc7150fd9685f6a5f4
Jahongir <bettaxacker@gmail.com>
2023-11-16 19:21:50 +0500

    qr code page created

---
+++ lib/screens/qr_screen/src/web/flutter_qr_stub.dart
@@ -0,0 +1,4 @@
+import 'package:components/screens/qr_screen/src/types/camera.dart';
+import 'package:flutter/material.dart';
+
+Widget createWebQrView({onPlatformViewCreated, CameraFacing? cameraFacing}) => const SizedBox();
