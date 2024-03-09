2b0503436554278d448b68d5fdb35270689aaa28
Khan <bettaxacker@gmail.com>
2024-03-09 11:47:59 +0500

    deleted trash files

--- lib/screens/qr_screen/src/web/flutter_qr_stub.dart
+++
@@ -1,4 +0,0 @@
-import 'package:components/screens/qr_screen/src/types/camera.dart';
-import 'package:flutter/material.dart';
-
-Widget createWebQrView({onPlatformViewCreated, CameraFacing? cameraFacing}) => const SizedBox();
